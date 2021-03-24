5acff07fc976f886baad9d2413d3009bb622b7a1
Marco Garcia Tammaro <mgt@greple.de>
2021-03-24 17:06:16 +0100

    Implement game controller

--- src/game/game.controller.spec.ts
+++ src/game/game.controller.spec.ts
@@ -1,20 +1,64 @@
-import {Test, TestingModule} from '@nestjs/testing';
-import {GameController} from './game.controller';
-import {GameService} from "./game.service";
+import { Test, TestingModule } from '@nestjs/testing';
+import { GameController } from './game.controller';
+import { GameService } from './game.service';
+import { BoardDto, GenerateGameDto, SolveResponseDto } from './dto';
+import { GreedyStrategy } from './domain/greedy-strategy';
 
 describe('GameController', () => {
-    let controller: GameController;
+  let controller: GameController;
+  let service: GameService;
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            controllers: [GameController],
-            providers: [GameService]
-        }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [GameController],
+      providers: [
+        GameService,
+        {
+          provide: 'PlayerStrategy',
+          useClass: GreedyStrategy,
+        },
+      ],
+    }).compile();
 
-        controller = module.get<GameController>(GameController);
-    });
+    controller = module.get<GameController>(GameController);
+    service = module.get<GameService>(GameService);
+  });
 
-    it('should be defined', () => {
-        expect(controller).toBeDefined();
-    });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should return new game board', () => {
+    const input = {
+      board: [
+        [1, 1],
+        [1, 1],
+      ],
+    } as BoardDto;
+    const spy = jest
+      .spyOn(service, 'generateGame')
+      .mockImplementation(() => input);
+    const actual: BoardDto = controller.generate({} as GenerateGameDto);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(actual).toBe(input);
+  });
+
+  it('should return the solve solution', () => {
+    const input = {
+      board: [
+        [1, 2],
+        [2, 3],
+      ],
+    } as BoardDto;
+    const expected = {
+      moveSequence: [1, 2, 3],
+      numberOfMoves: 3,
+    } as SolveResponseDto;
+    const spy = jest.spyOn(service, 'solve').mockImplementation(() => expected);
+    const actual: SolveResponseDto = controller.solve(input);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(actual).toBe(expected);
+  });
 });
--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,12 +1,21 @@
-import {Controller, Get} from '@nestjs/common';
-import {GameService} from "./game.service";
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { GameService } from './game.service';
+import { BoardDto, GenerateGameDto, SolveResponseDto } from './dto';
 
-@Controller('game')
+@Controller('/api/game')
 export class GameController {
-    constructor(private readonly gameService: GameService) {}
+  constructor(private readonly gameService: GameService) {}
 
-    @Get()
-    generate(): [] {
-        return this.gameService.generateBoard();
-    }
+  @Post('/generate')
+  @HttpCode(200)
+  generate(@Body() generateGameDto: GenerateGameDto): BoardDto {
+    generateGameDto = Object.assign(new GenerateGameDto(), generateGameDto);
+    return this.gameService.generateGame(generateGameDto);
+  }
+
+  @Post('/solve')
+  @HttpCode(200)
+  solve(@Body() board: BoardDto): SolveResponseDto {
+    return this.gameService.solve(board);
+  }
 }
